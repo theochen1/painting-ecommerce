021c5bd1fad3c5b319739616f7384fec82626864
theochen1 <theodore.chen.1@gmail.com>
2023-05-31 23:03:37 -0500

    modified:   package-lock.json 	modified:   package.json 	modified:   src/App.js 	new file:   src/components/Categories/index.js 	new file:   src/components/FeatureCard/index.js 	new file:   src/components/Footer/index.js 	new file:   src/components/Header/Header.css 	new file:   src/components/Header/index.js 	new file:   src/components/Hero/index.js 	new file:   src/components/ProductCard/index.js 	new file:   src/components/StatCard/index.js 	modified:   src/index.css 	modified:   src/index.js 	new file:   src/modules/Cart/index.js 	new file:   src/modules/CategoryProducts/index.js 	new file:   src/modules/Home/index.js 	new file:   src/modules/Product/index.js 	new file:   src/modules/Products/index.js 	new file:   tailwind.config.js

---
+++ src/components/FeatureCard/index.js
@@ -0,0 +1,67 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const FeatureCard = ({ cards = [1, 2, 3] }) => {
+  return (
+    <section className="text-gray-600 body-font">
+      <div className="container px-5 py-24 mx-auto">
+        <div className="flex flex-col text-center w-full">
+          <h2 className="text-xs text-indigo-500 tracking-widest font-medium title-font mb-1">
+            CATEGORIES
+          </h2>
+          <h1 className="sm:text-3xl text-2xl font-medium title-font text-gray-900 mb-20">
+            Our Popular Categories
+          </h1>
+        </div>
+        <div className="flex flex-wrap sm:-m-4 -mx-4 -mb-10 -mt-4 md:space-y-0 space-y-6">
+          {cards.map((card) => {
+            return (
+              <div className="p-4 md:w-1/3 flex">
+                <div className="w-12 h-12 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-4 flex-shrink-0">
+                  <svg
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    className="w-6 h-6"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M22 12h-4l-3 9L9 3l-3 9H2"></path>
+                  </svg>
+                </div>
+                <div className="flex-grow pl-6">
+                  <h2 className="text-gray-900 text-lg title-font font-semibold mb-2 capitalize">
+                    {card || "Example Card"}
+                  </h2>
+                  <p className="leading-relaxed text-base">
+                    Blue bottle crucifix vinyl post-ironic four dollar toast
+                    vegan taxidermy. Gastropub indxgo juice poutine, ramps
+                    microdosing banh mi pug VHS try-hard ugh iceland kickstarter
+                    tumblr live-edge tilde.
+                  </p>
+                  <Link to={`/categories/${card}`} className="mt-3 text-indigo-500 inline-flex items-center cursor-pointer">
+                    Learn More
+                    <svg
+                      fill="none"
+                      stroke="currentColor"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      className="w-4 h-4 ml-2"
+                      viewBox="0 0 24 24"
+                    >
+                      <path d="M5 12h14M12 5l7 7-7 7"></path>
+                    </svg>
+                  </Link>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default FeatureCard;
